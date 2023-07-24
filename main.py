c8e19efd6674e1aeefd02c0e5954c23c07dbe7c9
Davide Bancora <d.bancora@campus.unimib.it>
2023-07-24 16:51:49 +0200

    DAVIDE: - Creazione matrice di trasformazione

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import numpy as np
+import math
 
+
+# Funzione per la creazione della matrice di trasformazione
 def create_transformation_matrix(a):
+    # Calcolo della lunghezza dell'array
     n = len(a)
+
+    # Creazione del vettore alfa lungo quanto il vettore passato
     alpha = np.zeros(n)
+
+    # Calcolo dei valori in base alla posizione 
     alpha[0] = 1 / np.sqrt(n)
     alpha[1:] = np.sqrt(2/n)
 
-    print(alpha)
+    #DEBUG: print(alpha)
 
-    transformation_matrix = np.outer(alpha, a)
+    # Creazione della matrice di trasformazione
+    transformation_matrix = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            transformation_matrix[i, j] = alpha[i] * np.cos((math.pi * (2 * j + 1) * i) / (2 * n))
+    
     return transformation_matrix
 
 # Esempio di utilizzo:
 a = np.array([1, 2, 3, 4])
 transformation_matrix = create_transformation_matrix(a)
+# DEBUG: print (transformation_matrix)
